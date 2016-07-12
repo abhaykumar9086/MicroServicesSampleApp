c063138c944854885bbedeffb55c92a971d905f0
abhaykumar9086 <abhaymca78@gmail.com>
2016-07-12 14:52:53 +0530

    Add files via upload

---
+++ ProductService.Host/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ProductService.Host
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+            //api/Product/GetProductByProductID/{ID}
+
+            config.Routes.MapHttpRoute(
+             name: "GetProductByProductID",
+             routeTemplate: "api/{controller}/{ation}/{productID}",
+             defaults: new { productID = RouteParameter.Optional }
+         );
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+        }
+    }
+}

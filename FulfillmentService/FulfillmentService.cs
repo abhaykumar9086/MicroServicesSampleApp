11a49cff8866780da4789245e59d1f4d227e9a61
abhaykumar9086 <abhaymca78@gmail.com>
2016-07-12 14:33:15 +0530

    Add files via upload

---
+++ FulfillmentService/FulfillmentService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace FulfillmentService
+{
+    [Authorize]
+    public class FulfillmentService
+    {
+        [HttpGet]
+        public bool GetFullFillment(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
